4de0075db91cf27d47785798ffb22bea23297602
chstz365 <chstz_365@163.com>
2020-07-14 23:55:41 +0800

    update by chst365

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,6 +4,6 @@ describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js')
     console.log(baseConfig)
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, '/Users/zhangzhuang/Downloads/geektime-webpack-course-master/code/chapter05/my-project/builder-webpack/test/smoke/template/src/index/index.js')
+        assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js') > -1, true)
     })
 })
